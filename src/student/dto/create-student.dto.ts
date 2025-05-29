29fac407804d40f43687709d6c8f4625029ffb2a
getrude cheptoo <cheptoog677@gmail.com>
2025-05-29 16:13:48 +0300

    first commit

---
+++ src/student/dto/create-student.dto.ts
@@ -0,0 +1,50 @@
+// import 'reflect-metadata';
+// import { Type } from 'class-transformer';
+import { IsEmail, IsString, IsDate } from 'class-validator';
+// import { Type } from 'class-transformer';
+export class CreateStudentDto {
+  // @IsString()
+  // studentId: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+
+  // @Type(() => Date)
+  @IsDate()
+  dateOfBirth: Date;
+
+  @IsString()
+  gender: string;
+
+  @IsString()
+  phoneNumber: string;
+
+  @IsString()
+  address: string;
+
+  @IsString()
+  country: string;
+
+  @IsString()
+  state: string;
+
+  @IsString()
+  city: string;
+
+  // @Type(() => Date)
+  @IsDate()
+  enrollmentDate: Date;
+
+  @IsString()
+  status: string;
+
+  // @IsOptional()
+  @IsString()
+  profilePicture: string;
+}
